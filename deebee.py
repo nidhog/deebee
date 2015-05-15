b5a535872f1a591c3ac0f21756c8ff1586bddd2d
Ismail Elouafiq <ismail.elouafiq@telecom-bretagne.eu>
2015-05-15 15:31:41 +0200

    init stuff

--- deebee.py
+++ deebee.py
@@ -1,9 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+DeeBee
+deebee module
+ 
+@author: ielouafiq
+"""
+
+import sys
 import socket
 
-DEFAULT_HOST = 'localhost'
-DEFAULT_PORT = 50505
 socket_object = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 stats = {'PUT'	:{'success':0, 'error':0}, 
 		'GET'	:{'success':0, 'error':0}, 
 		'DELETE':{'success':0, 'error':0}
 		}
+
+def main():
+	pass
+	
+if __name__=="__main__":
+	sys.exit(main())
